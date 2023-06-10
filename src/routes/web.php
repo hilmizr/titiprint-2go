75f65d76a35265800e0ae642930048845565dd8d
mlintangp <lintangpanjerino.2002@gmail.com>
2023-06-10 17:32:59 +0700

    feat: add pesan routes

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PesanController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -33,6 +34,11 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/pesan', [PesanController::class, 'create'])->name('create-pesanan');
+    Route::post('/pesan', [PesanController::class, 'createAction']);
+    Route::get('/pesan/{id}', [PesanController::class, 'update'])->name('update-pesanan');
+    Route::post('/pesan/{id}', [PesanController::class, 'updateAction']);
 });
 
 require __DIR__.'/auth.php';
