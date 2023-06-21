ff5c9d47eaf112418e4277703733353f1b045b11
mlintangp <lintangpanjerino.2002@gmail.com>
2023-06-22 06:53:08 +0700

    fix: primary button bugs

--- src/resources/js/Pages/Home.jsx
+++ src/resources/js/Pages/Home.jsx
@@ -1,48 +1,51 @@
-import React from 'react';
-import './Home.module.css';
+// import React from "react";
+// // import './Home.module.css';
 
-function Home() {
-  return (
-    <div>
-      {/* Header */}
-      <header id="header" className="header fixed-top">
-        {/* Header code here */}
-      </header>
+// function Home() {
+//     return (
+//         <div>
+//             {/* Header */}
+//             <header id="header" className="header fixed-top">
+//                 {/* Header code here */}
+//             </header>
 
-      {/* Main Content */}
-      <main>
-        {/* Header */}
-        <section id="hero" className="hero d-flex align-items-center">
-          {/* Hero code here */}
-        </section>
+//             {/* Main Content */}
+//             <main>
+//                 {/* Header */}
+//                 <section id="hero" className="hero d-flex align-items-center">
+//                     {/* Hero code here */}
+//                 </section>
 
-        {/* About Us */}
-        <section id="about" className="py-5 d-flex align-items-center">
-          {/* About Us code here */}
-        </section>
+//                 {/* About Us */}
+//                 <section id="about" className="py-5 d-flex align-items-center">
+//                     {/* About Us code here */}
+//                 </section>
 
-        {/* Flow */}
-        <section id="flow" className="py-5 d-flex align-items-center">
-          {/* Flow code here */}
-        </section>
+//                 {/* Flow */}
+//                 <section id="flow" className="py-5 d-flex align-items-center">
+//                     {/* Flow code here */}
+//                 </section>
 
-        {/* Store Information */}
-        <section id="store" className="py-5 d-flex align-items-center">
-          {/* Store Information code here */}
-        </section>
+//                 {/* Store Information */}
+//                 <section id="store" className="py-5 d-flex align-items-center">
+//                     {/* Store Information code here */}
+//                 </section>
 
-        {/* Contact */}
-        <section id="contact" className="py-5 d-flex align-items-center">
-          {/* Contact code here */}
-        </section>
-      </main>
+//                 {/* Contact */}
+//                 <section
+//                     id="contact"
+//                     className="py-5 d-flex align-items-center"
+//                 >
+//                     {/* Contact code here */}
+//                 </section>
+//             </main>
 
-      {/* Footer */}
-      <footer className="mt-4 p-5 text-white">
-        {/* Footer code here */}
-      </footer>
-    </div>
-  );
-}
+//             {/* Footer */}
+//             <footer className="mt-4 p-5 text-white">
+//                 {/* Footer code here */}
+//             </footer>
+//         </div>
+//     );
+// }
 
-export default Home;
+// export default Home;
